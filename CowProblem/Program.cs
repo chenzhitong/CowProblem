d52dc16e5cf37d81b3af575e78582c4789af5707
陈志同 <chenzhitong12@hotmail.com>
2018-08-25 16:28:55 +0900

    commit

--- CowProblem/Program.cs
+++ CowProblem/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CowProblem
 {
@@ -6,7 +8,21 @@ namespace CowProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> initial = new List<int>() { 1, 0, 0 };
+            for (int i = 0; i < 50; i++)
+            {
+                var s = 0;
+                for (int j = 0; j < initial.Count; j++)
+                    if (j <= initial.Count - 3)
+                        s += initial[j];
+                initial.Add(s);
+            }
+            var sum = 0;
+            for (int i = 0; i < initial.Count; i++)
+            {
+                Console.WriteLine($"第{i + 1}年有{sum = sum + initial[i]}头");
+            }
+            Console.ReadLine();
         }
     }
 }
